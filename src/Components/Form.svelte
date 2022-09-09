3f78d221c6f58350c85b617136e33afcc0ec1b6a
Riley <riley.childs@yahoo.com>
2022-09-08 20:16:09 -0400

    Created logic for toggling reply and edit forms

---
+++ src/Components/Form.svelte
@@ -0,0 +1,18 @@
+<script>
+  import { currentUser } from "../comment-store";
+  export let formMode;
+</script>
+
+<form on:submit|preventDefault>
+  {#if formMode === "new-reply" || formMode === "new-comment"}
+    <img src={$currentUser[0].image.png} alt={$currentUser[0].username} />
+  {/if}
+  {#if formMode === "new-reply"}
+    <textarea name="" id="" cols="30" rows="10"> Add a reply... </textarea>
+  {:else if formMode === "new-comment"}
+    <textarea name="" id="" cols="30" rows="10"> Add a comment... </textarea>
+  {:else if formMode === "edit-content"}
+    <textarea name="" id="" cols="30" rows="10"> <slot/> </textarea>
+  {/if}
+  <button>Submit</button>
+</form>
