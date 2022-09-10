754d2b0443c3cbc4846c781c78ba9576d2388bee
Riley <riley.childs@yahoo.com>
2022-09-10 14:30:58 -0400

    Refactored code in CommentsBox.svelte

--- src/Comments/CommentBox.svelte
+++ src/Comments/CommentBox.svelte
@@ -5,21 +5,20 @@
 
 <section id="comment-box">
   {#each $comments as comment (comment.id)}
+    <Comment
+      id={comment.id}
+      commentOrReply="comment"
+      score={comment.score}
+      username={comment.user.username}
+      userImage={comment.user.image.png}
+      createdAt={comment.createdAt}
+      content={comment.content}
+      isCurrentUser={comment.user.username === $currentUser[0].username}
+    />
     <!-- If There Are Replies -->
     {#if comment.replies.length !== 0}
-      <!-- Checks for user comment -->
-        <Comment
-          id={comment.id}
-          commentOrReply="comment"
-          score={comment.score}
-          username={comment.user.username}
-          userImage={comment.user.image.png}
-          createdAt={comment.createdAt}
-          content={comment.content}
-          isCurrentUser={comment.user.username === $currentUser[0].username}
-        />
       <div class="reply-box">
-      {#each comment.replies as reply (reply.id)}
+        {#each comment.replies as reply (reply.id)}
           <Comment
             id={reply.id}
             commentOrReply="comment reply"
@@ -31,22 +30,8 @@
             content={reply.content}
             isCurrentUser={reply.user.username === $currentUser[0].username}
           />
-      {/each} 
-    </div>
-
-      <!-- If There Are No Replies -->
-    {:else}
-      <!-- Checks for user comment -->
-        <Comment
-          id={comment.id}
-          commentOrReply="comment"
-          score={comment.score}
-          username={comment.user.username}
-          userImage={comment.user.image.png}
-          createdAt={comment.createdAt}
-          content={comment.content}
-          isCurrentUser={comment.user.username === $currentUser[0].username}
-        />
+        {/each}
+      </div>
     {/if}
   {/each}
 </section>
