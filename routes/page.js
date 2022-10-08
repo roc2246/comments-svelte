eac24a9d190fce82a0e05cd2b5aa9cc967a82c97
Riley <riley.childs@yahoo.com>
2022-10-08 13:39:20 -0400

    Trimmed up code in page.js

--- routes/page.js
+++ routes/page.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router();
 
 router.get('*', (req, res) => {
-    return res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+   res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
   });
 
 module.exports = router
