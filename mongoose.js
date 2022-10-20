b225390b67cdeb80c1266f0e8873f4aa209c0274
Riley <riley.childs@yahoo.com>
2022-10-20 11:08:36 -0400

    Connected database to Atlas

--- mongoose.js
+++ mongoose.js
@@ -1,11 +1,15 @@
+const password = require('./private')
+
 const mongoose = require("mongoose");
-const dbPath = 'mongodb://127.0.0.1:27017/comments-section'
+const localDB = 'mongodb://127.0.0.1:27017/comments-section'
+const dbName = "comments"
+const MONGODB_URI = `mongodb+srv://childswebdev:${password}@comment-section-cluster.mn0pidu.mongodb.net/${dbName}?retryWrites=true&w=majority`
 require("dotenv").config(); 
 const db = mongoose.connection
 
 
 // Set up MongoDB
-mongoose.connect(dbPath, {
+mongoose.connect(MONGODB_URI || localDB, {
     useNewUrlParser: true
   })
 
